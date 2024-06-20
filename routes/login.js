b237dd59316741e9a59962277d86e80843001285
Kaychanne <michella.anastasya@itbs.ac.id>
2024-06-20 14:02:25 +0700

    finishing

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var connection = require('../lib/db');
+var bcrypt = require('bcrypt');
 
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Login' });
@@ -10,22 +11,32 @@ router.post('/', function(req, res, next) {
   var username = req.body.username;
   var password = req.body.password;
 
-  // Query the database for the user
   connection.query('SELECT * FROM user WHERE email = ?', [username], function(err, results) {
     if (err) {
       console.error('Database query error: ' + err.stack);
       return res.redirect('/login');
     }
 
-    if (results.length > 0 && results[0].password === password) {
+    if (results.length > 0) {
       var user = results[0];
-      if (user.email === 'admin@gmail.com') {
-        req.session.user = { username: user.email, role: 'admin' };
-        res.redirect('/form');
-      } else {
-        req.session.user = { username: user.email, role: 'user' };
-        res.redirect('/frontend');
-      }
+      bcrypt.compare(password, user.password, function(err, isMatch) {
+        if (err) {
+          console.error('Error comparing passwords: ' + err.stack);
+          return res.redirect('/login');
+        }
+
+        if (isMatch) {
+          if (user.email === 'admin@gmail.com') {
+            req.session.user = { username: user.email, role: 'admin' };
+            res.redirect('/form');
+          } else {
+            req.session.user = { username: user.email, role: 'user' };
+            res.redirect('/frontend');
+          }
+        } else {
+          res.redirect('/login');
+        }
+      });
     } else {
       res.redirect('/login');
     }
--- routes/register.js
+++ routes/register.js
@@ -1,19 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var connection = require('../lib/db');
+var bcrypt = require('bcrypt');
 
-router.post('/user', function(req, res) {
-    var { username, password } = req.body;
-    var hashedPassword = bcrypt.hashSync(password, 10);
-  
-    var sql = 'INSERT INTO users (username, password) VALUES (?, ?)';
-    connection.query(sql, [username, hashedPassword], function(err, result) {
+router.get('/', function(req, res, next) {
+  res.render('register', { title: 'Register' });
+});
+
+router.post('/', function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  // Hash the password before storing it in the database
+  bcrypt.hash(password, 10, function(err, hashedPassword) {
+    if (err) {
+      console.error('Error hashing password: ' + err.stack);
+      return res.redirect('/register');
+    }
+
+    var user = { email: username, password: hashedPassword };
+
+    // Insert the new user into the database
+    connection.query('INSERT INTO user SET ?', user, function(err, results) {
       if (err) {
-        res.status(500).send('Error registering user');
-      } else {
-        res.status(200).send('User registered successfully');
+        console.error('Database insertion error: ' + err.stack);
+        return res.redirect('/register');
       }
+
+      res.redirect('/login');
     });
   });
-  
-  module.exports = router;
+});
+
+module.exports = router;
